6176133a0d775047564ec1756e4c5bdf133687f6
nemitborn <mahir.muentes@bcestudent.com>
2026-09-22 16:49:21 +0100

    updated the creatures encounter

--- encounters.py
+++ encounters.py
@@ -118,8 +118,10 @@ def storm(player_stats):
                     time.sleep(2)
                     player_stats["members"]-=5
                     gameover(player_stats)
+                    print("*", player_stats["members"], "members left... *")
                     player_stats["morale"] -= 1
                     player_stats["trust"] -= 1
+                    print("* You can start to hear the angry whispers of the group behind you... *")
             else:
                 print("* The group starts to murmur... they do not trust your leadership in this. *")
                 time.sleep(2)
@@ -173,6 +175,8 @@ def creature(player_stats):
                 print("* You decided to just sacrifice some dude *")
                 player_stats["members"]-=1
                 gameover(player_stats)
+                print("*", player_stats["members"], "members left... *")
+                time.sleep(2)
                 player_stats["morale"]-=2
                 player_stats["trusts"]-=2
                 print("* The rest of your group sees your actions and fear for their own lives... *")
@@ -184,15 +188,31 @@ def creature(player_stats):
                 print("* Most of your group outruns the beast, however there are some that got left behind and eaten... *")
                 player_stats["members"]-=5
                 gameover(player_stats)
+                time.sleep(2)
+                print("*", player_stats["members"], "members left... *")
                 player_stats["morale"]-=1
+                time.sleep(2)
                 print("* The group seems to loose their fighting spirit even more... *")
         case 3:
             time.sleep(2)
-            if coinFlip()=="Pass" and player_stats["current_ammo"]>=10:
-                playsound("sound/gun1.mp3")
-                print("* You command your group to shoot at the beast... *")
-                time.sleep(2)
-                print("* it seems to run away in fear back into the trees... *")
-bandit_encounter(player_stats,resource_types)
-storm(player_stats)
+            if player_stats["current_ammo"]>=10:
+                if coinFlip()=="Pass":
+                    print("PASS")
+                    playsound("sound/gun1.mp3")
+                    print("* You command your group to shoot at the beast... *")
+                    time.sleep(2)
+                    print("* it seems to run away in fear back into the trees... *")
+                    player_stats["current_ammo"]-=10
+                else:
+                    print("FAIL")
+                    playsound("sounds/gun1.mp3")
+                    print("* You command your group to shoot at the beast... *")
+                    playsound("sound/gun2.mp3")
+                    print("* As you shoot the beast you see it pouncing on your members... *")
+                    player_stats["members"]-=3
+                    gameover(player_stats)
+                    print("*", player_stats["members"], "members left... *")
+                    player_stats["morale"]-=1
+                    print("* The group seems to loose their fighting spirit even more... *")
+
 creature(player_stats)
